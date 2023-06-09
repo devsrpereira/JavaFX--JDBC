002a0197d23fef70f9bb0aab02bbd53c237722af
devsrpereira <srpereira@uni9.edu.br>
2023-06-09 17:01:48 -0300

    Emementos de controle criados

--- JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/MainViewController.java
+++ JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/MainViewController.java
@@ -1,4 +1,41 @@
 package com.srdevpereira.javafx_jdbc;
 
-public class MainViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemVendedor;
+    @FXML
+    private MenuItem menuItemDepartamento;
+    @FXML
+    private MenuItem menuItemSobre;
+
+
+    @FXML
+    public void onMenuItemVendedorAction(){
+        System.out.println("onMenuItemVendedorAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartamentoAction(){
+        System.out.println("onmenuItemDepartamentoAction");
+    }
+
+    @FXML
+    public void onMenuItemSobreAction(){
+        System.out.println("onMenuItemSobreAction");
+    }
+
+
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) {
+
+    }
 }
