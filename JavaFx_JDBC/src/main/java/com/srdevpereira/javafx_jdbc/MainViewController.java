d637ec21f0ebdf2e89faeecfff130812abce8f35
devsrpereira <srpereira@uni9.edu.br>
2023-06-09 18:26:43 -0300

    Departamento view criado

--- JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/MainViewController.java
+++ JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/MainViewController.java
@@ -33,7 +33,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartamentoAction(){
-        System.out.println("onmenuItemDepartamentoAction");
+        loadView("DepartamentoList.fxml");
     }
 
     @FXML
