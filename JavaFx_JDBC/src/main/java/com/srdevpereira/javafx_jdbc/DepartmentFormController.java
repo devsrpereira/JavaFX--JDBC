312ba5a9eb966b2cc54698b0b8e3a8cf9781ec81
devsrpereira <srpereira@uni9.edu.br>
2023-06-11 14:27:13 -0300

    Salvar um novo departamento

--- JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/DepartmentFormController.java
+++ JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/DepartmentFormController.java
@@ -1,8 +1,14 @@
 package com.srdevpereira.javafx_jdbc;
 
+import com.srdevpereira.javafx_jdbc.db.DbException;
+import com.srdevpereira.javafx_jdbc.services.DepartmentService;
+import com.srdevpereira.javafx_jdbc.util.Alerts;
 import com.srdevpereira.javafx_jdbc.util.Constraints;
+import com.srdevpereira.javafx_jdbc.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,6 +19,7 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -25,24 +32,50 @@ public class DepartmentFormController implements Initializable {
     @FXML
     private Button btCancel;
 
-    public void setEntity(Department entity) {
+    public void setDepartment(Department entity) {
         this.entity = entity;
     }
-
-    @FXML
-    public void onBtSaveAction(){
-        System.out.println("onBtSaveAction");
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
     }
 
     @FXML
-    public void onBtCancelAction(){
-        System.out.println("onBtCancelAction");
+    public void onBtSaveAction(ActionEvent event){
+        if (entity == null){
+            throw new IllegalStateException("Entity estava nulo");
+        }
+        if (service == null){
+            throw new IllegalStateException("Service estava nulo");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        }
+        catch (DbException e){
+            Alerts.showAlerts("Error não foi possivel salvar", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
+
+    @FXML
+    public void onBtCancelAction(ActionEvent event){
+        Utils.currentStage(event).close();
+    }
+
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializeNodes();
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setNome(txtName.getText());
+
+        return obj;
     }
 
     private void initializeNodes(){
