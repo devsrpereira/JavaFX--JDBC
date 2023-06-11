c4adc1579344c5e80e90786f198da4b2e66e1715
devsrpereira <srpereira@uni9.edu.br>
2023-06-10 22:30:38 -0300

    Navegação entre a janela comun e a form

--- JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/DepartmentListController.java
+++ JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/DepartmentListController.java
@@ -1,16 +1,25 @@
 package com.srdevpereira.javafx_jdbc;
 
 import com.srdevpereira.javafx_jdbc.services.DepartmentService;
+import com.srdevpereira.javafx_jdbc.util.Alerts;
+import com.srdevpereira.javafx_jdbc.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -45,15 +54,15 @@ public class DepartmentListController implements Initializable {
 
 
     @FXML
-    public void onBtNovoDptoAction(){
-        System.out.println("onBtNovoDptoAction");
+    public void onBtNovoDptoAction(ActionEvent event){
+        Stage parentStage = Utils.currentStage(event);
+        createDialogForm("DepartmentForm.fxml",parentStage);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializeNode();
-        
-    }
+     }
 
     private void initializeNode() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
@@ -62,4 +71,23 @@ public class DepartmentListController implements Initializable {
         Stage stage = (Stage) MainViewApp.getMainScene().getWindow();
         tableViewDepartamentos.prefHeightProperty().bind(stage.heightProperty());
     }
+
+    private void createDialogForm(String absolutName, Stage parentStage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Entre com os dados do Departamento");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false); //não permite dimencionar a janela
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL); //impede a transição de janela com ela aberta
+            dialogStage.showAndWait();
+        }
+        catch (IOException e){
+            Alerts.showAlerts("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
 }
