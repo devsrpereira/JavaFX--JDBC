e30eab57368977f0e1eb58bbc151fdf72e12e976
devsrpereira <srpereira@uni9.edu.br>
2023-06-12 12:32:11 -0300

    TextField e DataPick do no sellerForm

--- JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/SellerFormController.java
+++ JavaFx_JDBC/src/main/java/com/srdevpereira/javafx_jdbc/SellerFormController.java
@@ -10,12 +10,11 @@ import com.srdevpereira.javafx_jdbc.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
 
 public class SellerFormController implements Initializable {
@@ -29,7 +28,19 @@ public class SellerFormController implements Initializable {
     @FXML
     private TextField txtName;
     @FXML
+    private TextField txtEmail;
+    @FXML
+    private DatePicker dpBirthDate;
+    @FXML
+    private TextField txtBaseSalary;
+    @FXML
     private Label labelErrorName;
+    @FXML
+    private Label labelErrorEmail;
+    @FXML
+    private Label labelErrorBirthDate;
+    @FXML
+    private Label labelErrorBaseSalary;
     @FXML
     private Button btSave;
     @FXML
@@ -44,7 +55,6 @@ public class SellerFormController implements Initializable {
     public void subscribeDataChangeListener(DataChangeListener listener){
         dataChangeListeners.add(listener);
     }
-
 
 
     @FXML
@@ -107,7 +117,10 @@ public class SellerFormController implements Initializable {
 
     private void initializeNodes(){
         Constraints.setTextFieldInteger(txtId);
-        Constraints.setTextFieldMaxLength(txtName, 30);
+        Constraints.setTextFieldMaxLength(txtName, 70);
+        Constraints.setTextFieldDouble(txtBaseSalary);
+        Constraints.setTextFieldMaxLength(txtEmail, 60);
+        Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
     }
 
     public void updateFormData(){
@@ -116,6 +129,12 @@ public class SellerFormController implements Initializable {
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getNome());
+        txtEmail.setText(entity.getEmail());
+        Locale.setDefault(Locale.US);
+        txtBaseSalary.setText(String.format("%.2f",entity.getBaseSalary()));
+        if(entity.getBirthDate() != null) {
+            dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault())); //instacia a data considerando o fuso do computadr do usuario
+        }
     }
 
     private void setErrorMessages(Map<String, String> error){
